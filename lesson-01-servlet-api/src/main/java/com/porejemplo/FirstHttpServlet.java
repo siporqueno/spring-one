52eae6741ba235dff52daa88af371dbeaa07e0eb
siporqueno <strike1000001@gmail.com>
2021-01-26 22:32:41 +0300

    classwork until FirstHttpServlet

---
+++ lesson-01-servlet-api/src/main/java/com/porejemplo/FirstHttpServlet.java
@@ -0,0 +1,24 @@
+package com.porejemplo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/first-http-servlet/*")
+public class FirstHttpServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Привет от сервлета!!!</h1>");
+        resp.getWriter().println("<p>contextPath: " + req.getContextPath() + "</p>");
+        resp.getWriter().println("<p>servletPath: " + req.getServletPath() + "</p>");
+        resp.getWriter().println("<p>pathInfo: " + req.getPathInfo() + "</p>");
+        resp.getWriter().println("<p>queryString: " + req.getQueryString() + "</p>");
+        resp.getWriter().println("<p>param1: " + req.getParameter("param1") + "</p>");
+        resp.getWriter().println("<p>param2: " + req.getParameter("param2") + "</p>");
+
+    }
+}
