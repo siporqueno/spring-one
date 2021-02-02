b38f3d9da12f87b6e1b9df846606f8f63b60be16
siporqueno <strike1000001@gmail.com>
2021-02-02 21:08:44 +0300

    Repeated classwork for entity Product. Created classes Product, ProductRepository, ProductController. Created views product and product_form. It works.

---
+++ lesson-03-spring-mvc/src/main/java/com/porejemplo/persist/Product.java
@@ -0,0 +1,40 @@
+package com.porejemplo.persist;
+
+public class Product {
+
+    private Long id;
+
+    private String title;
+
+    private int price;
+
+    public Product(String title, int price) {
+        this.id = -1L;
+        this.title = title;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
