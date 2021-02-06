39bbf7222d3d87596245295c678613b8da6330cb
siporqueno <strike1000001@gmail.com>
2021-02-06 11:30:34 +0300

    Changed @GetMapping for @DeleteMapping, changed link for form etc in ProductController and view product.html

--- lesson-03-spring-mvc/src/main/java/com/porejemplo/controller/ProductController.java
+++ lesson-03-spring-mvc/src/main/java/com/porejemplo/controller/ProductController.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/product")
@@ -62,7 +59,7 @@ public class ProductController {
         return "product_form";
     }
 
-    @GetMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     public String remove(@PathVariable("id") Long id) {
         logger.info("Delete endpoint requested for product with id {}", id);
         productRepository.delete(id);
