0d5a5b728d287d87eb5513ee67c693145d5c0aed
siporqueno <strike1000001@gmail.com>
2021-02-16 09:49:40 +0300

    copied project from lesson-04-spring-boot to lesson-07-spring-data

---
+++ lesson-07-spring-data/src/main/java/com/porejemplo/controller/ProductController.java
@@ -0,0 +1,75 @@
+package com.porejemplo.controller;
+
+import com.porejemplo.persist.Product;
+import com.porejemplo.persist.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public String listPage(Model model) {
+        logger.info("List page requested");
+
+        model.addAttribute("products", productRepository.findAll());
+        return "product";
+    }
+
+    @GetMapping("/{id}")
+    public String editPage(@PathVariable("id") Long id, Model model) {
+        logger.info("Edit page for id {} requested", id);
+
+        model.addAttribute("product", productRepository.findById(id));
+        return "product_form";
+    }
+
+    @PostMapping("/update")
+    public String update(@Valid Product product, BindingResult result) {
+        logger.info("Update endpoint requested");
+
+        if (result.hasErrors()){
+            return "product_form";
+        }
+
+        if (product.getId() != -1) {
+            logger.info("Updating product with id {}", product.getId());
+            productRepository.update(product);
+        } else {
+            logger.info("Creating new product");
+            productRepository.insert(product);
+        }
+        return "redirect:/product";
+    }
+
+    @GetMapping("/new")
+    public String create(Model model) {
+        logger.info("New endpoint requested");
+        model.addAttribute("product", new Product());
+        return "product_form";
+    }
+
+    @DeleteMapping("/{id}")
+    public String remove(@PathVariable("id") Long id) {
+        logger.info("Delete endpoint requested for product with id {}", id);
+        productRepository.delete(id);
+        return "redirect:/product";
+    }
+}
