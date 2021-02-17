d2d081af79640a8236743955901b555c84757a46
siporqueno <strike1000001@gmail.com>
2021-02-17 09:37:57 +0300

    Made service layer for entity Product

--- lesson-07-spring-data/src/main/java/com/porejemplo/controller/ProductController.java
+++ lesson-07-spring-data/src/main/java/com/porejemplo/controller/ProductController.java
@@ -1,7 +1,8 @@
 package com.porejemplo.controller;
 
 import com.porejemplo.persist.Product;
-import com.porejemplo.persist.ProductRepository;
+import com.porejemplo.service.ProductRepr;
+import com.porejemplo.service.ItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +19,18 @@ public class ProductController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
-    private ProductRepository productRepository;
+    private ItemService<ProductRepr> productService;
 
     @Autowired
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductController(ItemService<ProductRepr> productService) {
+        this.productService = productService;
     }
 
     @GetMapping
     public String listPage(Model model) {
         logger.info("List page requested");
 
-        model.addAttribute("products", productRepository.findAll());
+        model.addAttribute("products", productService.findAll());
         return "product";
     }
 
@@ -37,25 +38,20 @@ public class ProductController {
     public String editPage(@PathVariable("id") Long id, Model model) {
         logger.info("Edit page for id {} requested", id);
 
-        model.addAttribute("product", productRepository.findById(id));
+        model.addAttribute("product", productService.findById(id));
         return "product_form";
     }
 
     @PostMapping("/update")
-    public String update(@Valid Product product, BindingResult result) {
+    public String update(@Valid ProductRepr productRepr, BindingResult result) {
         logger.info("Update endpoint requested");
 
-        if (result.hasErrors()){
+        if (result.hasErrors()) {
             return "product_form";
         }
 
-        if (product.getId() != -1) {
-            logger.info("Updating product with id {}", product.getId());
-            productRepository.update(product);
-        } else {
-            logger.info("Creating new product");
-            productRepository.insert(product);
-        }
+        logger.info("Updating product with id {}", productRepr.getId());
+        productService.save(productRepr);
         return "redirect:/product";
     }
 
@@ -69,7 +65,7 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public String remove(@PathVariable("id") Long id) {
         logger.info("Delete endpoint requested for product with id {}", id);
-        productRepository.delete(id);
+        productService.delete(id);
         return "redirect:/product";
     }
 }
---
+++ lesson-07-spring-data/src/main/java/com/porejemplo/service/ItemService.java
@@ -0,0 +1,20 @@
+package com.porejemplo.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemService<T>{
+
+    List<T> findAll();
+
+    List<T> findWithFilterLike(String filterLike);
+
+    List<T> findWithFilterBetween(BigDecimal minFilter, BigDecimal maxFilter);
+
+    Optional<T> findById(long id);
+
+    void save(T item);
+
+    void delete(long id);
+}
