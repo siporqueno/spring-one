d1aada074de5b42265d8d4cdfd318256d5ed8609
siporqueno <strike1000001@gmail.com>
2021-02-23 10:22:14 +0300

    Previous lessons and Classwork of Lesson 9.

---
+++ lesson-09-rest-part-one/src/main/java/com/porejemplo/rest/UserResource.java
@@ -0,0 +1,93 @@
+package com.porejemplo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.porejemplo.controller.BadRequestException;
+import com.porejemplo.controller.NotFoundException;
+import com.porejemplo.service.UserRepr;
+import com.porejemplo.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserResource {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/all", produces = "application/json")
+    public List<UserRepr> findAll() {
+        return userService.findAll().stream()
+                .peek(u -> u.setPassword(null))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(path = "/{id}")
+    public UserRepr findById(@PathVariable("id") Long id) {
+        UserRepr userRepr = userService.findById(id)
+                .orElseThrow(NotFoundException::new);
+        userRepr.setPassword(null);
+        return userRepr;
+    }
+
+    @GetMapping("filter")
+    public Page<UserRepr> listPage(
+                           @RequestParam("usernameFilter") Optional<String> usernameFilter,
+                           @RequestParam("ageMinFilter") Optional<Integer> ageMinFilter,
+                           @RequestParam("ageMaxFilter") Optional<Integer> ageMaxFilter,
+                           @RequestParam("page") Optional<Integer> page,
+                           @RequestParam("size") Optional<Integer> size,
+                           @RequestParam("sortField") Optional<String> sortField) {
+
+        return userService.findWithFilter(
+                usernameFilter.orElse(null),
+                ageMinFilter.orElse(null),
+                ageMaxFilter.orElse(null),
+                page.orElse(1) - 1,
+                size.orElse(3),
+                sortField.orElse(null)
+        );
+    }
+
+    @PostMapping(consumes = "application/json")
+    public UserRepr create(@RequestBody UserRepr userRepr) {
+        if (userRepr.getId() != null) {
+            throw new BadRequestException();
+        }
+        userService.save(userRepr);
+        return userRepr;
+    }
+
+    @PutMapping(consumes = "application/json")
+    public void update(@RequestBody UserRepr userRepr) {
+        if (userRepr.getId() == null) {
+            throw new BadRequestException();
+        }
+        userService.save(userRepr);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> notFoundException(NotFoundException ex) {
+        return new ResponseEntity<>("Entity not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> badRequestException(BadRequestException ex) {
+        return new ResponseEntity<>("Bad request", HttpStatus.NOT_FOUND);
+    }
+}
